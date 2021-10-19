2a78d0af2a45605468fea7ed7af9e2be56b315c8
madhu <bamadhu1995@gmail.com>
2021-10-19 15:41:33 +0530

    import login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookingsComponent } from './bookings/bookings.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'bookings', component: BookingsComponent },
   { path: 'registration', component: RegistrationComponent },
+  { path: 'login', component: LoginComponent },
   {
     path: '',
     pathMatch: 'full',
