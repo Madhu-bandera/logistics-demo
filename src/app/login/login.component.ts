fc5e6d8172d6b4d2a28b2a764013a807bcfeb158
oknizam <oknizam321@gmail.com>
2021-10-28 20:01:02 +0530

    Changes are updated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,51 +3,56 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { CommonService } from '../providers/common-service/common.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  userLogin!: FormGroup;
+    userLogin!: FormGroup;
 
-  constructor(private fb: FormBuilder, private service: CommonService) {}
+    constructor(private fb: FormBuilder, private service: CommonService) { }
 
-  ngOnInit(): void {
-    this.userLogin = this.fb.group({
-      email: [
-        '',
-        [
-          Validators.required,
-          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
-        ],
-      ],
-      password: [
-        '',
-        [
-          Validators.required,
-          Validators.pattern(
-            '(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:{\\}\\[\\]\\|\\+\\-\\=\\_\\)\\(\\)\\`\\/\\\\\\]])[A-Za-z0-9d$@].{7,}'
-          ),
-        ],
-      ],
-    });
-  }
-  submit() {
-    console.log('Email', this.userLogin.controls.email.valid);
-    console.log('Password', this.userLogin.controls.password.valid);
-
-    if (this.userLogin.valid) {
-      console.log(this.userLogin.value);
-    } else {
-      alert('please enter email id & password');
+    ngOnInit(): void {
+        this.userLogin = this.fb.group({
+            email: [
+                '',
+                [
+                    Validators.required,
+                    Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+                ],
+            ],
+            password: [
+                '',
+                [
+                    Validators.required,
+                    Validators.pattern(
+                        '(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:{\\}\\[\\]\\|\\+\\-\\=\\_\\)\\(\\)\\`\\/\\\\\\]])[A-Za-z0-9d$@].{7,}'
+                    ),
+                ],
+            ],
+        });
     }
+    submit() {
 
-    const obj = {
-      email: 'oknizam321@gmail.com',
-      password: '1234',
-    };
-    this.service
-      .post('auth/login', obj)
-      .then((res) => console.log('response', res));
-  }
+
+        if (this.userLogin.valid) {
+            console.log(this.userLogin.value);
+            const obj = {
+                email: this.userLogin.value.email,
+                password: this.userLogin.value.password,
+            };
+            console.log('request object', obj);
+            this.service
+                .post('auth/login', obj)
+                .then((res: any) => {
+                    if (res && res.userId) {
+                        alert("User loggedIn!")
+                    }
+                });
+        } else {
+            alert('please enter email id & password');
+        }
+
+
+    }
 }
